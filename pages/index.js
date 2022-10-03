df643804a23d1c63a2837c254774a3e622e5fe61
Prajapati Jay <65757917+mysteriousmaster01@users.noreply.github.com>
2022-10-03 17:08:59 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -12,27 +12,38 @@ import {
 import Paragraph from "../components/paragraph";
 import Section from '../components/section';
 import NextLink from 'next/link';
-import { ChevronRightIcon } from "@chakra-ui/icons";
+import { DownloadIcon } from "@chakra-ui/icons";
 import { BioSection, BioYear } from "../components/bio";
 import Layout from "../components/layouts/article";
-import { IoLogoGithub, IoLogoInstagram,IoLogoGoogle, IoLogoWhatsapp } from 'react-icons/io5';
+import { IoLogoGithub, IoLogoInstagram, IoLogoGoogle, IoLogoWhatsapp } from 'react-icons/io5';
+import Typical from "react-typical";
 
 const Page = () => {
     return (
         <Layout>
-            <Container maxW='container.xl'>
-                <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mt={2} align="center">
+            <Container maxW='container-xl'>
+                {/* <Box borderRadius="lg" bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')} p={3} mt={3} align="center">
                     Hello There, Jay Prajapati here, Full Stack Developer In India
-                </Box>
+                </Box> */}
                 <Box display={{ md: 'flex' }} mt={3}>
                     <Box flexGrow={1}>
-                        <Heading as='h2' variant="page-title" align='center' fontFamily='mono'>Jay Prajapati</Heading>
-                        <p align='center'>Digital Craftman Developer / Artist / Designer </p>
+                        <Heading as='h2' variant="page-title" align='center' fontFamily='cursive'>Jay Prajapati</Heading>
+                        <Heading as='h4' align='center' mt={5} fontFamily=''>
+                            <span style={{color:'orangered'}}>
+                                <Typical
+                                    steps={["A Developer", 3000, "An Artist", 3000, "A Designer", 3000]}
+                                    loop={Infinity}
+                                    wrapper='i'
+                                    className='anitque'
+                                />
+                            </span> 
+                        </Heading>
+                        {/* <p align='center'>Developer / Artist / Designer </p> */}
 
                     </Box>
                     <Box flexShrink={0} mt={{ base: 4, md: 0 }} ml={{ md: 6 }} align='center'>
                         <Image
-                            maxWidth='150px'
+                            maxWidth='200px'
                             display='inline-block'
                             src="/images/Profile.png"
                             alt='profile img' />
@@ -40,17 +51,17 @@ const Page = () => {
                 </Box>
                 <Section delay={0.3}>
                     <Heading as='h3' variant='section-title' fontFamily='body'>
-                        Intro
+                        Objective
                     </Heading>
-                    <Paragraph>I am a 4th year Bachelors of Engineering Student in Information
-                        Technology at Sardar Vallabhbhai Patel Institute of Technology,
+                    <Paragraph>I am a Final Year Student Persuing Bachelor's of Engineering
+                        in Information Technology at Sardar Vallabhbhai Patel Institute of Technology,
                         Vasad. I’m seeking an opportunity to discover the professional
                         world that I want to integrate into after my studies .
                     </Paragraph>
                     <Box align='center' my={4}>
-                        <NextLink href='/works'>
-                            <Button rightIcon={<ChevronRightIcon />} colorScheme='telegram'>
-                                My Portfolio
+                        <NextLink href='/resume.pdf' target='_blank'>
+                            <Button rightIcon={<DownloadIcon />} colorScheme='telegram'>
+                                Resume
                             </Button>
                         </NextLink>
                     </Box>
